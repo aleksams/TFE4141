a00ba19e4b82cbfd4f98e004f9ccc1912616f9d9
aleksams <35399103+aleksams@users.noreply.github.com>
2018-10-04 09:51:48 +0200

    Update RSA_Ctrl.vhd

--- rtl/RSA_Ctrl.vhd
+++ rtl/RSA_Ctrl.vhd
@@ -1,1 +1,19 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use work.all;
 
+entity RSA_Ctrl is
+  port(
+    -- input
+    clk: in std_ulogic;
+    -- output
+  );
+end RSA_Ctrl;
+  
+  
+  
+architecture behaviour of RSA_Ctrl is
+  -- internal signals
+begin -- logic
+
+end behaviour;
