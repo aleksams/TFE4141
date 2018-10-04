ffbb583b364a0e4e53e35a0afa7abb305fe6701d
aleksams <35399103+aleksams@users.noreply.github.com>
2018-10-04 09:51:11 +0200

    Update RSA_Core.vhd

--- rtl/RSA_Core.vhd
+++ rtl/RSA_Core.vhd
@@ -1,1 +1,19 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use work.all;
 
+entity RSA_Core is
+  port(
+    -- input
+    clk: in std_ulogic;
+    -- output
+  );
+end RSA_Core;
+  
+  
+  
+architecture behaviour of RSA_Core is
+  -- internal signals
+begin -- logic
+
+end behaviour;
