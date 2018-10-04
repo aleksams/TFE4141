5ee59442e674862a99a3a6074385842e7163058b
aleksams <35399103+aleksams@users.noreply.github.com>
2018-10-04 09:49:38 +0200

    Update RSA.vhd

--- rtl/RSA.vhd
+++ rtl/RSA.vhd
@@ -1,8 +1,11 @@
-
+library ieee;
+use ieee.std_logic_1164.all;
+use work.all;
 
 entity RSA is
   port(
     -- input
+    clk: in std_ulogic;
     -- output
   );
 end RSA;
@@ -10,7 +13,8 @@ end RSA;
   
   
 architecture behaviour of RSA is
-  --internal signals
+  -- internal signals
 begin -- logic
-
+  -- include RSA_Core
+  -- include RSA_Ctrl
 end behaviour;
