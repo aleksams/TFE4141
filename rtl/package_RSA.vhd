2f79e4d9ec33bb07236bd7bf965d95357056a273
aleksams <35399103+aleksams@users.noreply.github.com>
2018-10-04 09:29:05 +0200

    Update package_RSA.vhd with constants and states

--- rtl/package_RSA.vhd
+++ rtl/package_RSA.vhd
@@ -1,1 +1,7 @@
 
+package package_RSA is
+  constant RESET_VAL : "0"; -- Not sure if this is right
+  constant DATA_WIDTH : integer := 32;
+  constant MESSAGE_WIDTH : integer := 256; -- Legal: {128, 256}
+  type STATE : (RESET, IDLE, ACTIVE); -- Just an example
+end package_RSA;
